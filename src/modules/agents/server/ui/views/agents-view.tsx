ab092bd53d194300712a335121d74f4e5fa3b8b6
Alice Leung <aliceleung34@gmail.com>
2025-06-27 22:06:43 -0400

    Created new component for CommandResponsiveDialog

--- src/modules/agents/server/ui/views/agents-view.tsx
+++ src/modules/agents/server/ui/views/agents-view.tsx
@@ -3,7 +3,6 @@ import {useSuspenseQuery} from "@tanstack/react-query";
 import {useTRPC} from "@/trpc/client";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
-
 export const AgentsView = () =>{
     const trpc = useTRPC();
     const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions());
