6cb843db369ed49231dbef164795ddf951f7748a
Alice Leung <aliceleung34@gmail.com>
2025-07-06 22:41:20 -0400

    changed meetings view and updated procedures meeting data with agent information

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -1,8 +1,8 @@
 import { db } from "@/db";
-import {meetings} from "@/db/schema";
+import {agents, meetings} from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import {z} from "zod";
-import {and, count, desc, eq, getTableColumns, ilike} from "drizzle-orm";
+import {and, count, desc, eq, getTableColumns, ilike, sql} from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/contants";
 import { TRPCError } from "@trpc/server";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
@@ -71,14 +71,22 @@ export const meetingsRouter = createTRPCRouter({
         const data = await db
         .select({    
             ...getTableColumns(meetings),
+            agent: agents,
+            duration:sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration"),
         })
-        .from(meetings).where(and(
+        .from(meetings)
+        .innerJoin(agents,eq(meetings.agentID, agents.id))
+        .where(and(
             eq(meetings.userID, ctx.auth.user.id),
             search ?ilike(meetings.name , '%${search}%') :undefined,
         )
     ).orderBy(desc(meetings.createdAt),desc(meetings.id)).limit(pageSize).offset((page-1)*pageSize)
     
-    const [total] = await db.select({count:count()}).from(meetings).where(and(  eq(meetings.userID, ctx.auth.user.id),
+    const [total] = await 
+    db.select({count:count()})
+    .from(meetings)
+    .innerJoin(agents,eq(meetings.agentID, agents.id))
+    .where(and(  eq(meetings.userID, ctx.auth.user.id),
             search ?ilike(meetings.name , '%${search}%') :undefined,
         )
     );
