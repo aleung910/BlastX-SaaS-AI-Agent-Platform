a0b8fc7cfd2db6e7c1b82796d31e2de4ca93ec3c
Alice Leung <aliceleung34@gmail.com>
2025-07-22 17:43:08 -0500

    Created subscription page with detailed pricing options and added sidebar component for free trial usage

--- src/components/generated-avatar.tsx
+++ src/components/generated-avatar.tsx
@@ -1,4 +1,4 @@
-import {createAvatar} from "@dicebear/core";
+import { createAvatar } from "@dicebear/core";
 import {botttsNeutral, initials} from "@dicebear/collection";
 import {cn} from "@/lib/utils";
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
